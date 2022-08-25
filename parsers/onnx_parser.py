36472bf570d71feca46c7b839125a69be8a18a65
KonstantinChernyak <chernyakonstantin@gmail.com>
2022-08-25 03:08:48 +0300

    Update ONNX parser

--- parsers/onnx_parser.py
+++ parsers/onnx_parser.py
@@ -1,12 +1,17 @@
+"""
+The module contains a parser for PyTorch models.
+The parser parse ONNX files.
+"""
+
 import json
 
 import numpy as np
 import onnx
 from google.protobuf.json_format import MessageToJson
-from onnx import shape_inference, TensorProto, ValueInfoProto
+from onnx import shape_inference
 
 
-class Parser:
+class ONNXParser:
     def __init__(self, model_path: str):
         self.model_path = model_path
         self.onnx_model = onnx.load(self.model_path)
@@ -17,36 +22,23 @@ class Parser:
         onnx_json = json.loads(s)
         adj_mat = self.get_adj_mat(onnx_json)
         op_type = self.get_op_types(onnx_json)
-        in_shapes = self.get_input_shapes(onnx_json)
         out_shapes = self.get_output_shapes(onnx_json)
-        return op_type, in_shapes, out_shapes, adj_mat
-
-    def get_input_shapes(self, onnx_json: dict) -> list:
-        in_shapes = []
-        for node in onnx_json['graph']['node']:
-            in_shapes_ = []
-            for input_name in node['input']:
-                input_info = (list(filter(lambda x: x.name == input_name, self.inferred_model.graph.value_info))
-                              + list(filter(lambda x: x.name == input_name, self.inferred_model.graph.input))
-                              + list(filter(lambda x: x.name == input_name, self.inferred_model.graph.initializer)))
-                input_spec = next(iter(input_info))
-                if isinstance(input_spec, TensorProto):
-                    in_shape = list(input_spec.dims)
-                elif isinstance(input_spec, ValueInfoProto):
-                    in_shape = [int(dim.dim_value) for dim in input_spec.type.tensor_type.shape.dim]
-                in_shapes_.append(in_shape)
-            in_shapes.append(in_shapes_)
-        return self.align_shapes(in_shapes)
+        return op_type, out_shapes, adj_mat
 
     def get_output_shapes(self, onnx_json: dict) -> list:
         out_shapes = []
         for node in onnx_json['graph']['node']:
             out_shapes_ = []
             for output_name in node['output']:
-                output_info = (list(filter(lambda x: x.name == output_name, self.inferred_model.graph.value_info))
-                               + list(filter(lambda x: x.name == output_name, self.inferred_model.graph.output)))
+                output_info = (list(filter(lambda x: x.name == output_name,
+                                           self.inferred_model.graph.value_info))
+                               + list(filter(lambda x: x.name == output_name,
+                                             self.inferred_model.graph.output)))
+                if not output_info:
+                    continue
                 output_spec = next(iter(output_info))
-                out_shape = [int(dim.dim_value) for dim in output_spec.type.tensor_type.shape.dim]
+                out_shape = [int(dim.dim_value) for dim in
+                             output_spec.type.tensor_type.shape.dim]
                 out_shapes_.append((out_shape))
             out_shapes.append(out_shapes_)
         return self.align_shapes(out_shapes)
@@ -59,7 +51,10 @@ class Parser:
         for i, node_1 in enumerate(nodes):
             outputs: list = node_1['output']
             for j, node_2 in enumerate(nodes):
-                inputs: list = node_2['input']
+                try:
+                    inputs: list = node_2['input']
+                except:
+                    inputs = []
                 for output in outputs:
                     if output in inputs:
                         adj[j, i] = 1
@@ -86,4 +81,3 @@ class Parser:
             for _ in range(n_absent_nodes):
                 shapes[i].append([0] * max_dim)
         return shapes
-        
